c9ec842e46e98d41bea28f44843d8269db464ef5
Chengcheng <genius52@qq.com>
2017-05-04 09:52:32 +0800

    Fix regex issue

--- FileCompare.py
+++ FileCompare.py
@@ -1,21 +1,78 @@
 import filecmp
 import difflib
 import sys
+import re
 
-def CompareSqlFile(oldSql,newSql):
-    # oldFile = open(oldSql,'r')
-    # newFile = open(newSql,'r')
-    if oldSql == "" or newSql=="":
-        print "Usage: test.py filename1 filename2"
-        sys.exit()
-    text1_lines = readline(oldSql)
-    text2_lines = readline(newSql)
-    d = difflib.HtmlDiff()
-    print d.make_file(text1_lines,text2_lines)
+def FindRelatedTable(s,filecontent):
+    pattern = r".*create table (\S+).*%s.*\)" % s.replace('(',"\(").replace(')',"\)")
+    regex = re.compile(pattern,re.DOTALL)
+    m = regex.match(filecontent)
+    if m:
+        print m.group(1)
+        return m.group(1)
+    return None
 
-def readline(filename):
-    fileHandle = open (filename,'rb')
-    text = fileHandle.read().splitlines()
-    fileHandle.close()
-    return text
+def GenerateDeleteAttributeSql(table,attr):
+    pattern = r"(\S+)\s+(\S+)\s\S.*"
+    m = re.match(pattern,attr.strip())
+    if m:
+        print "Delete" ,m.group(1)
+        sql = "alter table %s drop column %s;" % (table,m.group(1))
+        return sql
+    return None
+
+def GenerateAddAttributeSql(table,attr):
+    pattern = r"(\S+)\s+(\S+)\s\S.*"
+    m = re.match(pattern,attr.strip())
+    if m:
+        print "Add" ,m.group(1)
+        sql = "alter table %s add column %s %s;" % (table,m.group(1),m.group(2))
+        return sql
+    return None
+
+def GenerateModifyAttributeSql(s):
+    print "Modify",s.strip()
+    return
+
+def GenerateDeleteTableSql(s):
+    print "Delete table",s.strip()
+    return
+
+def GenerateAddTableSql(s):
+    print "Add table",s.strip()
+    return
+
+def GenerateUpgradeSql(oldSql,newSql):
+    try:
+        oldFile = open(oldSql,'r')
+        newFile = open(newSql,'r')
+        if oldSql == "" or newSql == "":
+            print "Usage: test.py filename1 filename2"
+            sys.exit()
+        text1_lines = oldFile.read().splitlines()
+        text2_lines = newFile.read().splitlines()
+        oldlines = open(oldSql, 'r').read()
+        newlines = open(newSql, 'r').read()
+        diff = difflib.ndiff(text1_lines,text2_lines)
+        upgradeSql = 'use rt_tms;\n'
+        for d in list(diff):
+            if d.find('-') == 0:#'delete attribute in old sql'
+                tablename = FindRelatedTable(d[1:].strip(),oldlines)
+                if tablename != None:
+                    sentence = GenerateDeleteAttributeSql(tablename,d[1:])
+                    if sentence != None:
+                        upgradeSql = upgradeSql + sentence + '\n'
+            elif d.find('+') == 0:
+                tablename = FindRelatedTable(d[1:].strip(), newlines)
+                if tablename != None:
+                    sentence = GenerateAddAttributeSql(tablename,d[1:])
+                    if sentence != None:
+                        upgradeSql = upgradeSql + sentence + '\n'
+            elif d.find('?') == 0:
+                print "unclear modify exist ",d
+        return upgradeSql
+    except:
+        oldFile.close()
+        newFile.close()
+        return None
 
--- Main.py
+++ Main.py
@@ -1,16 +1,32 @@
 #coding=utf-8
 import os
 import FileCompare
+import re
+
+# def TestRegex(s,filecontent):
+#     pattern = r".*create table (\S+).*%s.*" % s.replace('(',"\(").replace(')',"\)")
+#     regex = re.compile(pattern,re.DOTALL)
+#     m = regex.match(filecontent)
+#     if m:
+#         print m.groups(1)
+#         return m.groups(1)#     return None
 
 if __name__ == '__main__':
-    print "Start upgrade DB"
-    oldSql = "D:/RT_TMS/MAIN/Scripts/TSS/AppServer/InstallScripts/TMSDB.sql"
-    newSql = "D:/RT_TMS/Doc/DataModel/TMSDB.sql"
-    bcExe = "\"C:/Program Files (x86)/Beyond Compare 3/BCompare.exe\""
-    diffFile = "\"@D:/diff.txt\""
-    resultFile = "\"D:/BCompare.HTML\""
+    oldSql = "D:\diff1.txt"
+    newSql = "D:\diff2.txt"
+    upgradeSql = "D:\upgrade.sql"
 
-    FileCompare.CompareSqlFile(oldSql,newSql)
+    f = open("d:/difftest.txt",'r')
+    s = r"uid                  varchar(64)"
+    content = f.read()
+    # TestRegex(s,content)
+
+    sql = FileCompare.GenerateUpgradeSql(oldSql,newSql)
+    if len(sql) > 0:
+        f = open(upgradeSql,"wt")
+        f.write(sql)
+        f.close()
+    print sql
     # command = bcExe,"/silent",diffFile,oldSql,newSql,resultFile
     # ret = os.system(command)
     # print ret
@@ -19,3 +35,4 @@ if __name__ == '__main__':
 #Compare with the previous sql
 #Generate DB upgrade sql
 #Update system data convertor code
+
