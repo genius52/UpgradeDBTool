f6d39e8d92e76e2b16e3d240396df04634d9ea58
Chengcheng <genius52@qq.com>
2017-06-13 10:46:14 +0800

    Add tmsmachine table for CRC

--- GlobalSetting.py
+++ GlobalSetting.py
@@ -4,4 +4,4 @@ New_Tmssql = "D:\RT_TMS\Doc\DataModel\TMSDB.sql"
 Dbwrapper_Design = "D:\RT_TMS\MAIN\Features\Projects\UIH.RT.Framework.DBWrapper\RTDBModel.Designer.cs"
 Systemdata_Datamodel = "D:\RT_TMS\MAIN\Features\Projects\UIH.RT.Framework.DataModel\BackupRestore\DM_DbObj.cs"
 Systemdata_Convertor = "D:\RT_TMS\MAIN\Features\Projects\UIH.RT.TMS.AppServer\Converters\DbObjectConvertor.cs"
-ProtectedTables = ("<tmsdeliverybeamgroup>", "<tmstreatmentbeam>", "<tmscontrolpoint>","<tmsbeamrecord>","<tmscontrolpointrecord>", "<tmsprescription>", "<tmspatient>","<tmssite>")
+ProtectedTables = ("<tmsdeliverybeamgroup>", "<tmstreatmentbeam>", "<tmscontrolpoint>","<tmsbeamrecord>","<tmscontrolpointrecord>", "<tmsprescription>", "<tmspatient>","<tmssite>","<tmsmachine>")
